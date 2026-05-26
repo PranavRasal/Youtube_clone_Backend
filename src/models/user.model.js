25c23ee0940b524f930da3a4cff61d46106c7059
PranavRasal <pranavcode19@gmail.com>
2026-05-27 02:04:53 +0530

    feat: add user authentication with JWT and bcrypt, implement multer for file uploads, and configure Cloudinary for image storage

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose , {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
 
@@ -19,7 +21,7 @@ const userSchema = new Schema({
         trim : true
     },
 
-    fullname:{
+    fullName:{
         type :String ,
         required : true ,
         trim : true ,
@@ -54,5 +56,58 @@ const userSchema = new Schema({
 },{
     timestamps : true
 })
+
+// pre save hook to hash the password before saving  the user document to the database 
+//  compare the password when we are trying to login the user   
+
+
+userSchema.pre("save" , async function (next){ 
+/*  this refers to when we create a new user document or update the password of 
+    an existing user document , then only we will hash the password and save it to
+    the database with the help of bcrypt package */
+
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password , 10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    // this refers to the user document for which we are checking the password
+   return await bcrypt.compare(password , this.password);
+}
+
+//jwt package is used to generate access token and refresh token for the user document and
+//  we will use the secret key and options from the environment variables to generate 
+// the tokens
+
+userSchema.methods.generateAccessToken = function(){ 
+/*  this refers to the user document for which we are generating the token with
+    the help jwt package we can generate the token by using the sign method of jwt
+    and we need to pass the payload and secret key and options like expiresIn */
+
+    return  jwt.sign(
+        {   _id : this._id     ,  username : this.username , // payload
+            email : this.email , fullName : this.fullName  },
+
+        process.env.ACCESS_TOKEN_SECRET , // secret key
+
+      { expiresIn : process.env.ACCESS_TOKEN_EXPIRES  } // options
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    // this refers to the user document for which we are generating the token  
+
+    return  jwt.sign(
+        {   _id : this._id     ,  username : this.username , // payload
+            email : this.email , fullName : this.fullName  },
+
+        process.env.REFRESH_TOKEN_SECRET , // secret key
+
+      { expiresIn : process.env.REFRESH_TOKEN_EXPIRES  } // options
+    )
+}
+
+
 
 export const User = mongoose.model("User" , userSchema)
