3f49afd738c33adaa004b8728519d7437dad6579
PranavRasal <pranavcode19@gmail.com>
2026-06-01 03:07:12 +0530

    fix: correct dotenv path in index.js and export apiResponse correctly in apiResponse.js, add apiError class for error handling

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import dns from "dns";
 dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 dotenv.config({
-    path : './env'
+    path : './.env'
 });
 
 connectDB()
--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -7,4 +7,4 @@ class apiResponse {
   }
 }
 
-export {aipResponse};
+export { apiResponse };
